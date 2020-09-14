989dd6cfa4bb7a1818ac1cd9e28312ae12737362
kefflen <55823300+kefflen@users.noreply.github.com>
2020-09-14 09:22:37 -0300

    (14-09) ordenação

---
+++ estDados/bubbleSort.js
@@ -0,0 +1,27 @@
+
+
+function bubbleSort(vetor) {
+    let trocas
+    let passadas = 0, comparacoes = 0, totalTrocas = 0
+    do {
+        passadas++
+        trocas = 0
+        //Ate o penultimo
+        for (let i=0; i <= vetor.length - 2; i++) {
+            comparacoes++
+            if (vetor[i] > vetor[i+1]) {
+                [vetor[i], vetor[i + 1]] = [vetor[i + 1], vetor[i]]
+                trocas++
+            
+            }
+
+        }
+        totalTrocas += trocas
+    } while (trocas > 0)
+     console.log({passadas, comparacoes, totalTrocas})
+}
+let nums = [58, 16, 33, 82, 4, 47, 25, 71, 96, 60, 41, 89]
+console.time("Teste1")
+bubbleSort(nums)
+console.timeEnd("Teste1")
+console.log(nums)
